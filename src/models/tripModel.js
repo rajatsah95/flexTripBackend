4167ec678968b7a532f064ce708b01f2b387ad68
rajatsah95 <rajatsah95@gmail.com>
2025-11-28 11:34:58 +0530

    flexTripBackend added 7

--- src/models/tripModel.js
+++ src/models/tripModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const tripSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: String, required: true },
   message: { type: String, required: true },
 });
 
